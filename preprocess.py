8f5a9ee987d2609b5fea2a46db21022fe77a8ce5
Keshav <keshavgogia57@gmail.com>
2025-05-15 02:55:04 +0530

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,16 +1,9 @@
-# preprocess.py
 from PIL import Image
 import numpy as np
 
 def preprocess_image(img):
-    # Convert to grayscale
-    img = img.convert("L")
-    
-    # Resize to 256x256
+    img = img.convert("L")  
     img = img.resize((256, 256))
-    
-    # Convert to numpy array and normalize
-    img_array = np.array(img) / 255.0
-    
-    # Add batch and channel dimensions (shape becomes [1, 256, 256, 1])
-    return np.expand_dims(img_array, axis=(0, -1))
+    arr = np.array(img) / 255.0  
+    arr = np.expand_dims(arr, axis=(0, -1))  
+    return arr.astype(np.float32)
